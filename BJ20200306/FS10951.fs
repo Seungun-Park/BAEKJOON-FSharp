aadb4027b83e3c483dfe8d791f73eb2b576582d5
sucystem <sucystem@outlook.com>
2020-03-06 07:09:38 +0900

    20200306 01

---
+++ BJ20200306/FS10951.fs
@@ -0,0 +1,12 @@
+﻿open System
+open System.Text
+let outputStr = new StringBuilder()
+let mutable tf = true
+while tf do
+    let test = Console.ReadLine()
+    if test = null then
+        tf <- false
+    else
+        let arr = test.Split ' '
+        (arr.[0] |> int) + (arr.[1] |> int) |> string |> outputStr.AppendLine |> ignore
+Console.Write outputStr
---
+++ BJ20200306/FS10952.fs
@@ -0,0 +1,11 @@
+﻿open System
+open System.Text
+let outputStr = new StringBuilder()
+let mutable tf = true
+while tf do
+    let arr = Console.ReadLine().Split ' '
+    if arr = [|"0"; "0"|] then
+        tf <- false
+    else
+        (arr.[0] |> int) + (arr.[1] |> int) |> string |> outputStr.AppendLine |> ignore
+Console.Write outputStr
