cf559ae9054d90569e702f8120d264729424a601
sucystem <sucystem@outlook.com>
2020-03-06 07:41:30 +0900

    20200306 02

---
+++ BJ20200306/FS1110.fs
@@ -0,0 +1,19 @@
+﻿open System
+let mutable x = 0
+
+let proces (n:int) =
+    let mutable count = 1
+    if n < 10 then
+        x <- n * 10 + n
+    else
+        x <- (n % 10) * 10 + ((n / 10) + (n % 10)) % 10
+    while x <> n do
+        x <- (x % 10) * 10 + ((x / 10) + (x % 10)) % 10
+        count <- count + 1
+    count
+
+[<EntryPoint>]
+let main argv =
+    let n = Console.ReadLine() |> int
+    Console.Write (proces n)
+    0
