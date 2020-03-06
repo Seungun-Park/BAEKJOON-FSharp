c49b3c938013d464eed4b89a6acdf06ef9ff02ed
sucystem <sucystem@outlook.com>
2020-03-06 15:03:03 +0900

    20200306 03

---
+++ BJ20200306/FS10818.fs
@@ -0,0 +1,8 @@
+﻿open System
+Console.ReadLine() |> ignore
+Console.ReadLine().Split ' '
+|> Array.map int
+|> fun arr -> [Array.min arr; Array.max arr]
+|> List.map string
+|> String.concat " "
+|> Console.Write
---
+++ BJ20200306/FS2562.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+let maxNum list =
+    let rec search x mi i li =
+        match li with
+        | head::tail when head > x -> search head i (i + 1) tail
+        | head::tail when head < x -> search x mi (i + 1) tail
+        | [] -> mi
+    let idx = search 0 0 0 list
+    [list.[idx]; idx + 1]
+
+[<EntryPoint>]
+let main argv =
+    [for _ in 1 .. 9 do yield (Console.ReadLine() |> int)]
+    |> maxNum
+    |> List.map string
+    |> String.concat "\n"
+    |> Console.Write
+    0
