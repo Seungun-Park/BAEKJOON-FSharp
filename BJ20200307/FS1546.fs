687eb8bc0672f1fdbcbb0dd5d63937c1da417034
sucystem <sucystem@outlook.com>
2020-03-07 23:19:11 +0900

    20200307 03

---
+++ BJ20200307/FS1546.fs
@@ -0,0 +1,13 @@
+﻿open System
+let count arr =
+    let m:double = Array.max arr
+    [for i in 0 .. (arr.Length - 1) do yield ((arr.[i] / m) * 100.0)]
+    |> List.average
+[<EntryPoint>]
+let main argv =
+    Console.ReadLine() |> ignore
+    Console.ReadLine().Split ' '
+    |> Array.map double
+    |> count
+    |> Console.Write
+    0
---
+++ BJ20200307/FS8958.fs
@@ -0,0 +1,23 @@
+﻿open System
+open System.Text
+
+let outputStr = StringBuilder()
+
+let score (str:char List) =
+    let rec count s cons pre l =
+        match l with
+        | head::tail when head = 'O' -> count (s+cons) (cons+1) head tail
+        | head::tail when head = 'X' -> count s 1 head tail
+        | [] -> s
+    count 0 1 'X' str |> string
+
+[<EntryPoint>]
+let main argv = 
+    for _ in 1 .. (Console.ReadLine() |> int) do 
+        Console.ReadLine()
+        |> Seq.toList
+        |> score
+        |> outputStr.AppendLine
+        |> ignore
+    Console.Write outputStr
+    0 
