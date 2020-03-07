1b034f2f1a880440d34e0d0b6571c4a763101691
sucystem <sucystem@outlook.com>
2020-03-07 22:15:37 +0900

    20200307 02

---
+++ BJ20200307/FS3052.fs
@@ -0,0 +1,19 @@
+﻿open System
+let count (l:int List) =
+    let rec subcount (cnt:int) li =
+        match li with
+            | head::tail -> 
+                try
+                    List.find (fun x -> x = head) tail |> ignore
+                    subcount cnt tail
+                with
+                    | _ -> subcount (cnt + 1) tail
+            | [] -> cnt
+    subcount 0 l
+[<EntryPoint>]
+let main argv =
+    [for _ in 1 .. 10 do yield ((Console.ReadLine() |> int) % 42)]
+    |> List.map int
+    |> count
+    |> Console.Write
+    0
