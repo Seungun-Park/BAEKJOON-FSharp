33c7a2c1612679dd63fa524d23c0c98a7ae34347
sucystem <sucystem@outlook.com>
2020-03-07 11:54:09 +0900

    20200307 01

---
+++ BJ20200307/FS2557.fs
@@ -0,0 +1,18 @@
+﻿open System
+let count (x:int) =
+    let cnt = Array.zeroCreate 10
+    let rec numcount (num:int) =
+        if num < 10 then
+            cnt.[num] <- cnt.[num] + 1
+        else
+            cnt.[num%10] <- cnt.[num%10] + 1
+            numcount (num/10)
+    numcount x |>ignore
+    cnt
+[for _ in 1 .. 3 do yield Console.ReadLine()]
+|> List.map int
+|> fun x -> x.[0] * x.[1] * x.[2]
+|> count
+|> Array.map string
+|> String.concat "\n"
+|> Console.Write
