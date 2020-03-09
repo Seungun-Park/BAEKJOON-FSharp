5bcf56113cb7abe18d97839fb2359a49dec719f5
sucystem <sucystem@outlook.com>
2020-03-09 21:28:01 +0900

    20200309 01

---
+++ BJ20200309/FS11654.fs
@@ -0,0 +1,2 @@
+﻿open System
+Console.Read() |> Console.Write
---
+++ BJ20200309/FS11720.fs
@@ -0,0 +1,8 @@
+﻿open System
+Console.ReadLine() |> ignore
+Console.ReadLine()
+|> Seq.toList
+|> List.map string
+|> List.map int
+|> List.sum
+|> Console.Write
