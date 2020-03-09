141a746fe53d23116eeb0c4d8ee009cdf6372d89
sucystem <sucystem@outlook.com>
2020-03-09 21:20:25 +0900

    error fix

--- BJ20200308/FS4344.fs
+++ BJ20200308/FS4344.fs
@@ -1,7 +1,14 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System
+let aver inp =
+    let arr = inp |> Array.tail |> Array.map float
+    let avg = Array.average arr
+    let student = Array.filter (fun x -> x > avg) arr |> Array.length
+    (student |> float) / (arr.Length |> float) * 100.0
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    [for _ in 1 .. (Console.ReadLine() |> int) do yield (Console.ReadLine().Split ' ' |> aver)]
+    |> List.map (sprintf "%.3f%%")
+    |> String.concat "\n"
+    |> Console.Write
+    0
