8280ab8633f906a25b8b719f3a255bcb8b9fd4f5
sucystem <sucystem@outlook.com>
2020-03-10 22:10:06 +0900

    20200310 01

---
+++ BJ20200310/FS10809.fs
@@ -0,0 +1,6 @@
+﻿open System
+let s = Console.ReadLine() |> Seq.toList
+[for i in 'a' .. 'z' do yield (try s |> List.findIndex (fun x -> x = i) with _ -> -1)]
+|> List.map string
+|> String.concat " "
+|> Console.Write
---
+++ BJ20200310/FS2675.fs
@@ -0,0 +1,16 @@
+﻿open System
+let makeStr (str:String) =
+    let arr = str.Split ' ' |> Array.map string
+    let rec substr x cnt l =
+        match l with
+        | head::tail when cnt < x -> string(head) + substr x (cnt+1) l
+        | head::tail when cnt = x -> string(head) + substr x 1 tail
+        | [] -> ""
+    substr (arr.[0] |> int) 1 (arr.[1] |> Seq.toList)
+[<EntryPoint>]
+let main argv =
+    [for _ in 1 .. int(Console.ReadLine()) do yield Console.ReadLine() |> makeStr]
+    |> List.map string
+    |> String.concat "\n"
+    |> Console.Write
+    0
