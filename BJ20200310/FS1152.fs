f8cc04025793460495114e3e7ce4ea10a3289aaf
sucystem <sucystem@outlook.com>
2020-03-10 23:00:13 +0900

    20200310 02

---
+++ BJ20200310/FS1152.fs
@@ -0,0 +1,10 @@
+﻿open System
+Console.ReadLine().Split()
+|> fun x ->
+    if x.[0] = "" && x.[x.Length - 1] = "" then
+        x.Length - 2
+    else if x.[0] = "" || x.[x.Length - 1] = "" then
+        x.Length - 1
+    else
+        x.Length   
+|> Console.Write
---
+++ BJ20200310/FS1157.fs
@@ -0,0 +1,13 @@
+﻿open System
+let s = Console.ReadLine().ToUpper() |> Seq.toList |> List.map int
+let arr : int array = Array.zeroCreate 26
+for e in s do arr.[e-65] <- arr.[e-65] + 1
+let mutable c = 'a'
+let mutable cnt = 0
+for i in 0 .. 25 do
+    if arr.[i] > cnt then
+        c <- char(i + 65)
+        cnt <- arr.[i]
+    else if arr.[i] = cnt then
+        c <- '?'
+c |> Console.Write
