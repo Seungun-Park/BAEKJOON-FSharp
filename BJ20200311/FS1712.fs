2a57d355353de6c7ffa43560d12515972875c359
sucystem <sucystem@outlook.com>
2020-03-11 18:18:36 +0900

    20200311 03

---
+++ BJ20200311/FS1712.fs
@@ -0,0 +1,11 @@
+﻿open System
+let point a b c =
+    if (c - b) <= 0 then -1
+    else (a / (c - b)) + 1
+[<EntryPoint>]
+let main argv = 
+     Console.ReadLine().Split ' '
+     |> Array.map int
+     |> fun x -> point x.[0] x.[1] x.[2]
+     |> Console.Write
+     0
