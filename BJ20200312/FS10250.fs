4902459e0efae54ad9819bc0ba27f7f30dff08ce
sucystem <sucystem@outlook.com>
2020-03-12 18:22:28 +0900

    20200312 02

---
+++ BJ20200312/FS10250.fs
@@ -0,0 +1,15 @@
+﻿open System
+let calc w n =
+    let di = n / w
+    let mo = n % w
+    if mo = 0 then
+        w * 100 + di
+    else
+        mo * 100 + di + 1
+[<EntryPoint>]
+let main argv = 
+    [for _ in 1 .. int(Console.ReadLine()) do yield (Console.ReadLine().Split ' ' |> Array.map int |> fun x -> calc x.[0] x.[2])]
+    |> List.map string
+    |> String.concat "\n"
+    |> Console.Write
+    0
---
+++ BJ20200312/FS1193.fs
@@ -0,0 +1,15 @@
+﻿open System
+let rec calc floor x target =
+    let number = floor + x
+    let order = target - x
+    if number >= target then
+        if (floor % 2) = 0 then
+            [order; floor - order + 1]
+        else
+            [floor - order + 1; order]
+    else
+        calc (floor + 1) number target
+[<EntryPoint>]
+let main argv =
+    Console.ReadLine() |> int |> calc 1 0 |> List.map string |> String.concat "/" |> Console.Write
+    0
