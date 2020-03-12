62143e4e449653dbb0c2ace01b9b7b1406ee2b13
sucystem <sucystem@outlook.com>
2020-03-12 18:00:49 +0900

    20200312 01

---
+++ BJ20200312/FS2292.fs
@@ -0,0 +1,13 @@
+﻿open System
+let honeycomb t =
+    let rec count target ans x =
+        let tmp = 6 * ans + x
+        if tmp >= target then
+            ans + 1
+        else
+            count target (ans + 1) tmp
+    count t 0 1
+[<EntryPoint>]
+let main argv = 
+    Console.ReadLine() |> int |> honeycomb |> Console.Write
+    0
---
+++ BJ20200312/FS2869.fs
@@ -0,0 +1,5 @@
+﻿open System
+Console.ReadLine().Split ' '
+|> Array.map int
+|> fun x -> (x.[2] - x.[0]) / (x.[0] - x.[1]) + 1
+|> Console.Write
