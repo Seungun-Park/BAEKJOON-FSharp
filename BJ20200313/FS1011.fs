5b1fa7c6ba3fd51362c73cb16efedb99f334a120
sucystem <sucystem@outlook.com>
2020-03-13 13:57:11 +0900

    20200313 01

---
+++ BJ20200313/FS1011.fs
@@ -0,0 +1,14 @@
+﻿open System
+let calc (x:double) =
+    let n = sqrt x |> ceil
+    if ((n*n) + ((n-1.0)*(n-1.0))) / 2.0 < x then
+        n * 2.0 - 1.0 |> uint64
+    else
+        n * 2.0 - 2.0 |> uint64
+[<EntryPoint>]
+let main argv = 
+     [for _ in 1 .. int(Console.ReadLine()) do yield Console.ReadLine().Split ' ' |> Array.map double |> fun x -> calc (x.[1] - x.[0])]
+     |> List.map string
+     |> String.concat "\n"
+     |> Console.Write
+     0
---
+++ BJ20200313/FS2775.fs
@@ -0,0 +1,16 @@
+﻿open System
+let apt = Array2D.create 15 14 0
+let apt_init =
+    for i in 1 .. 14 do
+        apt.[0, i - 1] <- i
+        apt.[i, 0] <- 1
+    for i in 1 .. 14 do
+        for j in 1 .. 13 do
+            apt.[i, j] <- apt.[i, j - 1] + apt.[i - 1, j]
+[<EntryPoint>]
+let main argv = 
+    [for _ in 1 .. int(Console.ReadLine()) do yield apt.[int(Console.ReadLine()), int(Console.ReadLine()) - 1]]
+    |> List.map string
+    |> String.concat "\n"
+    |> Console.Write
+    0
