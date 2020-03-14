f0e6e21feaeba8074255619fb69eb67b9a0d254e
sucystem <sucystem@outlook.com>
2020-03-14 17:01:32 +0900

    20200314 01

---
+++ BJ20200314/FS1978.fs
@@ -0,0 +1,17 @@
+﻿open System
+let arr : bool array = Array.create 1001 true
+[<EntryPoint>]
+let main argv = 
+    arr.[0] <- false
+    arr.[1] <- false
+    for i in 2 .. 1000 do
+        if arr.[i] then
+            for j in (i + i) .. i .. 1000 do
+                arr.[j] <- false
+    Console.ReadLine() |> ignore
+    Console.ReadLine().Split ' '
+    |> Array.map int
+    |> Array.filter (fun x -> arr.[x])
+    |> Array.length
+    |> Console.Write
+    0
