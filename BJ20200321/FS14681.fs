048515e3fbdb5ec2de113689590e647d4a9c0280
sucystem <sucystem@outlook.com>
2020-03-21 22:32:18 +0900

    20200321

---
+++ BJ20200321/FS14681.fs
@@ -0,0 +1,13 @@
+﻿open System
+[<EntryPoint>]
+let main argv = 
+    [|Console.ReadLine() |> int; Console.ReadLine() |> int|]
+    |> fun x ->
+        match x with
+        | [|x; y|] when x > 0 && y > 0 -> 1
+        | [|x; y|] when x < 0 && y > 0 -> 2
+        | [|x; y|] when x < 0 && y < 0 -> 3
+        | [|x; y|] when x > 0 && y < 0 -> 4
+        | _ -> 0
+    |> Console.Write
+    0
