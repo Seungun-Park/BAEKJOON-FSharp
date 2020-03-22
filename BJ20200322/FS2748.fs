f44bb05bf7b0fb8a2fa55f7aa0b405412d7b157a
sucystem <sucystem@outlook.com>
2020-03-22 20:21:46 +0900

    20200322

---
+++ BJ20200322/FS2748.fs
@@ -0,0 +1,13 @@
+﻿open System
+let rec fibo n x (pre1 : uint64) (pre2 : uint64) =
+    if n = x then pre1 + pre2
+    else fibo n (x + 1) pre2 (pre1 + pre2)
+[<EntryPoint>]
+let main argv = 
+    Console.ReadLine() |> int
+    |> fun x ->
+        if x = 0 then 0 |> uint64
+        else if x = 1 then 1 |> uint64
+        else fibo x 2 (0 |> uint64) (1 |> uint64)
+    |> Console.Write
+    0
